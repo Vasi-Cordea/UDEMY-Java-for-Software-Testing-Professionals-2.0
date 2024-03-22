9afd34d1faf7240c0c877eb817433575d32836f2
Vasi-Cordea <150058199+Vasi-Cordea@users.noreply.github.com>
2024-03-22 11:12:42 +0200

    return param

--- UDEMY_Java_for_Software_Testing_Pros/src/OOP/Calculations.java
+++ UDEMY_Java_for_Software_Testing_Pros/src/OOP/Calculations.java
@@ -2,24 +2,25 @@ package OOP;
 
 public class Calculations {
 
-	int num1;
+	int num1; // parameters
 	int num2;
 	int sum, product;
 
-	void calculate(int num1, int num2) {  // function or method
+	int calculate(int num1, int num2) { // function or method
 		sum = num1 + num2;
-		product = num1 * num2;
+		return sum;
 
-		System.out.println("sum is: " + sum);
-		System.out.println("product is: " + product);
+		// product = num1 * num2;
+		// System.out.println("sum is: " + sum);
+		// System.out.println("product is: " + product);
 	}
 
 	public static void main(String[] args) {
-		
-		Calculations cal = new Calculations();  // object instantiations
-		cal.calculate(3,2);
-		cal.calculate(5,5);
-		cal.calculate(9,22);
-	
+		int result;
+		Calculations cal = new Calculations(); // object instantiations
+	//	cal.calculate(3, 2);
+
+		result = cal.calculate(2, 2) + 5;
+		System.out.println(result);
 	}
 }
