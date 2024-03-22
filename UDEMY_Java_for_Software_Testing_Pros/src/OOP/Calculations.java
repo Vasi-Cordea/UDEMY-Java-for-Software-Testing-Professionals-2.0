d49c8833bd0c3bf26262d4f029966aa4a79317e9
Vasi-Cordea <150058199+Vasi-Cordea@users.noreply.github.com>
2024-03-22 12:02:46 +0200

    object inst 2

--- UDEMY_Java_for_Software_Testing_Pros/src/OOP/Calculations.java
+++ UDEMY_Java_for_Software_Testing_Pros/src/OOP/Calculations.java
@@ -2,25 +2,22 @@ package OOP;
 
 public class Calculations {
 
-	int num1; // parameters
+	int num1;
 	int num2;
 	int sum, product;
 
-	int calculate(int num1, int num2) { // function or method
+	void calculate() {  // function or method
 		sum = num1 + num2;
-		return sum;
+		product = num1 * num2;
 
-		// product = num1 * num2;
-		// System.out.println("sum is: " + sum);
-		// System.out.println("product is: " + product);
+		System.out.println("sum is: " + sum);
+		System.out.println("product is: " + product);
 	}
 
 	public static void main(String[] args) {
-		int result;
-		Calculations cal = new Calculations(); // object instantiations
-	//	cal.calculate(3, 2);
-
-		result = cal.calculate(2, 2) + 5;
-		System.out.println(result);
+		
+		Calculations cal = new Calculations();  // object instantiations
+		cal.calculate();
+	
 	}
 }
