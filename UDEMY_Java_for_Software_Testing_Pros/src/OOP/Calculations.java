1c6b55446b5f0fed1a30941766e2db27cc9a2f34
Vasi-Cordea <150058199+Vasi-Cordea@users.noreply.github.com>
2024-03-22 10:33:26 +0200

    calculations instantiate

--- UDEMY_Java_for_Software_Testing_Pros/src/OOP/Calculations.java
+++ UDEMY_Java_for_Software_Testing_Pros/src/OOP/Calculations.java
@@ -1,21 +1,23 @@
 package OOP;
 
 public class Calculations {
-	
+
 	int num1 = 4;
 	int num2 = 3;
 	int sum, product;
-void calculate() {
-	sum = num1 + num2;
-	product = num1 * num2;
 
-	System.out.println("sum is: " + sum);
-	System.out.println("product is: " + product);
-}
+	void calculate() {  // function or method
+		sum = num1 + num2;
+		product = num1 * num2;
+
+		System.out.println("sum is: " + sum);
+		System.out.println("product is: " + product);
+	}
+
 	public static void main(String[] args) {
-
-
 		
-
-}
+		Calculations cal = new Calculations();  // object instantiations
+		cal.calculate();
+		cal.calculate();
+	}
 }
